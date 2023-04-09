8fc63bd830be87b4ae6a79b39aa3669e1c71c15e
Beed <0xbeedu@gmail.com>
2023-04-09 20:12:06 +0200

    BeedGPT

---
+++ js/js-chatgpt-clone/README.md
@@ -0,0 +1,13 @@
+# chatGPT clone
+
+### clone chatGPT with javascript
+
+<br>
+
+Using chatGPT API. Get your API Key [here](https://platform.openai.com/account/api-keys). <br>
+To use the API, make sure that you have enough [credits](https://platform.openai.com/account/usage)
+
+<br>
+
+### The code was made following the tuto of [AsmrProg](https://www.youtube.com/watch?v=05ssqx-SZT0)
+
---
+++ js/js-chatgpt-clone/app.js
@@ -0,0 +1,48 @@
+const API_KEY = '' //
+const submitButton = document.querySelector('#submit')
+const outPutElement = document.querySelector('#output')
+const inPutElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+function changeInput(value) {
+    const inputElement = document.querySelector('input')
+    inputElement.value = value
+}
+
+async function getMessage() {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: inPutElement.value}]
+        })
+    }
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        console.log(data)
+        outPutElement.textContent = data.choices[0].message.content
+        if(data.choices[0].message.content && inPutElement.value){
+            const pElement = document.createElement('p')
+            pElement.textContent = inPutElement.value
+            pElement.addEventListener('click',()=> changeInput(pElement.textContent))
+            historyElement.append(pElement)
+        }
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+submitButton.addEventListener('click',getMessage)
+
+function clearInput() {
+    inPutElement.value = ''
+}
+
+buttonElement.addEventListener('click',clearInput)
